a239c8f3c944e724a409a7701e0ec9134273b4ca
vuthuhatk4 <107761556+vuthuhatk4@users.noreply.github.com>
2023-09-27 23:39:52 +0700

    Add files via upload

---
+++ backend/src/lambda/http/createTodo.ts
@@ -0,0 +1,34 @@
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import 'source-map-support/register'
+import * as middy from 'middy'
+import { cors, httpErrorHandler } from 'middy/middlewares'
+import { CreateTodoRequest } from '../../requests/CreateTodoRequest'
+import { getUserId } from '../utils';
+import { createTodo } from '../../businessLogic/todos'
+
+export const handler = middy(
+  async (event: APIGatewayProxyEvent): Promise<APIGatewayProxyResult> => {
+    const newTodo: CreateTodoRequest = JSON.parse(event.body)
+
+    const userId = getUserId(event);
+    const newItem = await createTodo(newTodo, userId);
+    return {
+      statusCode: 201,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true
+      },
+      body: JSON.stringify({
+        item: newItem
+      })
+    };
+  }
+);
+
+handler
+  .use(httpErrorHandler())
+  .use(
+    cors({
+      credentials: true
+    })
+  );
